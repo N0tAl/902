09059d328e8de2d9a2e81496a74de54cded2c63f
N0tAl <118183654+N0tAl@users.noreply.github.com>
2024-09-09 02:47:58 +0300

    Add files via upload

--- test/js/scroll.js
+++ test/js/scroll.js
@@ -18,23 +18,40 @@ window.addEventListener('scroll', function() {
     });
 });
 
+function scrollNav() {
+  // Привязываем обработчик клика только для ссылки с id="studentLink"
+  $('#studentLink').click(function(event){
+    event.preventDefault();  // Предотвращаем переход по ссылке
 
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    touchDrag: true, // Включение перетаскивания на сенсорных экранах
-    mouseDrag: true, // Включение перетаскивания мышью
-    pullDrag: true,  // Позволяет тянуть с отскоком
-    freeDrag: true,  // Свободная перетаскиваемость
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 3
-      }
-    }
+
+
+    // Плавно прокручиваем к блоку с ID "S"
+    $('html, body').stop().animate({
+      scrollTop: $('#S').offset().top - 160  // Прокручиваем к якорю с отступом 160px
+    }, 500);  // Длительность анимации 500 мс
   });
+}
+
+// Вызываем функцию при загрузке страницы
+scrollNav();
+
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    }
+  }
 });
+
+
+function openMenu() {
+  const nav = document.querySelector('nav');
+  nav.classList.toggle('open');
+}
